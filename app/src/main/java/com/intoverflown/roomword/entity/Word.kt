0eb4fe92b431f49e3e98c5731ee01b7f51ee39a5
RocqJones <jonesmbindyo@gmail.com>
2022-08-14 17:17:09 +0300

    Create Entity

---
+++ app/src/main/java/com/intoverflown/roomword/entity/Word.kt
@@ -0,0 +1,17 @@
+package com.intoverflown.roomword.entity
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+/**
+ * Step 1. Create Entity
+ * @PrimaryKey Every entity needs a primary key. To keep things simple, each word acts as its own primary key.
+ * @ColumnInfo(name = "word") Specifies the name of the column in the table if you want it to be different
+ * from the name of the member variable. This names the column "word".
+ */
+@Entity(tableName = "word_table")
+class Word(
+    @PrimaryKey(autoGenerate = true) val id : Int,
+    @ColumnInfo(name = "word") val word: String
+)
