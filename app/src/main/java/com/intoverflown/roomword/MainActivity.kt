ebbe9e761e71c3f9080109c955c92fc3cab36ed3
RocqJones <jonesmbindyo@gmail.com>
2022-08-16 12:59:01 +0300

    init adapter

--- app/src/main/java/com/intoverflown/roomword/MainActivity.kt
+++ app/src/main/java/com/intoverflown/roomword/MainActivity.kt
@@ -2,10 +2,21 @@ package com.intoverflown.roomword
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.intoverflown.roomword.adapters.WordListAdapter
+import com.intoverflown.roomword.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var binding : ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding!!.root)
+
+        val adapter = WordListAdapter()
+        binding!!.recyclerview.adapter = adapter
+        binding!!.recyclerview.layoutManager = LinearLayoutManager(this)
     }
 }
